9e5c015e255ff72b1119210b0a310e2edc436be2
Bruno Matos Silva <bmsdevfullstack@gmail.com>
2022-06-18 14:06:01 -0300

    state setlikecount

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -15,7 +15,9 @@ export function Comment({ content, onDeleteComment }: any) {
     }
 
     function handleLikeComment() {
-        setLikeCount(likeCount + 1);
+        setLikeCount((state) => {
+            return state + 1
+        });
     }
 
     return (
